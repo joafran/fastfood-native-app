f044181008c3d5d1f70cd4e9872723a24275d729
joafran <joafran0016@gmail.com>
2021-06-01 12:25:11 -0300

    trying something

--- screens/NewOrder.js
+++ screens/NewOrder.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 const NewOrder = () => {
-    return ( 
-        <Text>NewOrder</Text>
+    return (
+        <View>
+            <Text>NewOrder</Text>
+        </View>
      );
 }
  
