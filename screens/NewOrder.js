152196d3ffe66467aefaa5e49cc71c8587126e3c
joafran <joafran0016@gmail.com>
2021-06-01 14:32:57 -0300

    updated neworder screen and created globalstyles

--- screens/NewOrder.js
+++ screens/NewOrder.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Button, Text } from 'react-native-paper'
+import globalStyles from '../styles/global';
+import { useNavigation } from '@react-navigation/native'
 
 const NewOrder = () => {
+
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>NewOrder</Text>
+        <View style={globalStyles.container}>
+            <View style={[globalStyles.content, style.content]}>
+                <Button
+                    mode="contained"
+                    style={globalStyles.button}
+                    onPress={() => navigation.navigate('Menu')}
+                >
+                    <Text style={globalStyles.buttonText}>Make an order</Text>
+                </Button>
+            </View>
         </View>
      );
 }
- 
+
+const style = StyleSheet.create({
+    content: {
+        flexDirection: 'column',
+        justifyContent: 'center'
+    }
+})
+
 export default NewOrder;
---
+++ styles/global.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    content: {
+        marginHorizontal: '2.5%',
+        flex: 1,
+    },  
+    button: {
+        backgroundColor: '#FFDA00',
+        borderRadius: 30
+    },
+    buttonText: {
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        color: '#000'
+    }
+});
+
+export default globalStyles;
